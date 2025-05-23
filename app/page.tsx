e3b7914779af46e3129cabd50bb0523a9d9dceab
Nathaniel Meyer <nathaniel.lorne@gmail.com>
2025-05-23 12:12:09 -0600

    removed auth on accident. Lets see if this works

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,58 @@ import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 import FileUploader from "@/components/fileuploader";
+import {generateClient} from "aws-amplify/data" 
+import type { Schema } from "@/amplify/data/resource";
+import { Authenticator } from "@aws-amplify/ui-react";
 
 Amplify.configure(outputs);
 
-export default function Page() {
-  // however you obtain your entity_id:
-  const entity_id = "YOUR_ENTITY_ID_HERE";
+Amplify.configure({
+  Auth: {
+    Cognito: {
+
+    // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+    identityPoolId: 'ca-central-1:ffbd856b-eac4-4af3-9d04-5b04eee9e989', 
+
+    // OPTIONAL - Amazon Cognito User Pool ID
+    userPoolId: 'ca-central-1_URmo5iTYo',
+
+    // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+    userPoolClientId: '6ulhpadeqg7f8cb7u8c2m03t98',
+
+    loginWith: { email: false, username: true }
+
+    }
+  }
+});
+
+const client = generateClient<Schema>();
+
+export default function App() {
 
   return (
-    <main>
-      <h1>Upload a File</h1>
-      <FileUploader entity_id={entity_id} />
-    </main>
+
+    <Authenticator>
+
+      {({ signOut, user }) => (
+
+        <main>
+
+          {user && (
+
+            <h1>Welcome, {user.username}</h1>
+
+          )}
+
+          <button onClick={signOut}>Sign out</button>
+
+          <h1>Upload a File</h1>
+          {user && <FileUploader entity_id={user.username} />}
+        </main>
+
+      )}
+
+    </Authenticator>
+
   );
 }
